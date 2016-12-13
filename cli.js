b4f9deb818ff33a3b23299841bad28c23699db80
TonyUCLA <TonyUCLA>
2016-12-12 18:54:28 -0800

    Files created and Psuedo Code Written

---
+++ cli.js
@@ -0,0 +1,17 @@
+var inquirer = require('inquirer');
+
+// input inquirer UI here.
+// User should be able to create a Basic Flashcard (front, back)
+
+    // (front) is the question.
+    // (back) has the answer.
+// User should be able to create Cloze Flashcard (text, cloze)
+
+    // (text) is the question with ellipses.
+        // ex. (...)who is the best?
+    // (cloze) is the hidden answer included into the question.
+
+/* For completion: 
+    - User must be able to save created flashcards to a text file.
+    - Store at least one property, 
+    - Equip cloze flashcards with at least one additional method. */
