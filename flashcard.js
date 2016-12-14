1c7aac29625e9c25dabf31fb0889552a59251e7d
TonyUCLA <TonyUCLA>
2016-12-14 14:25:01 -0800

    Basic Flashcard creation

---
+++ flashcard.js
@@ -0,0 +1,28 @@
+var inquirer = require('inquirer');
+
+
+function Flashcard(front, back) {
+    var flashCards = [];
+    this.front = front;
+    this.back = back;
+    this.createCard = function() {
+        inquirer.prompt([
+        {
+            type: "input",
+            message: "Front text",
+            name: "frontText"
+        },
+        {
+            type: "input",
+            message: "Back text",
+            name: "backText"
+        }
+        ]).then(function(input){
+            var newFlash = new Flashcard(input.front, input.back);
+                flashCards.push(newFlash);
+        });
+    }
+
+}
+
+module.exports = Flashcard;
